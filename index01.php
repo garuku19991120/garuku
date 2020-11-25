2aff60ef84d66e47cc2cf7f7a44301b06b543dc1
t-1120 <r_togai@itccl.jp>
2020-11-25 11:09:33 +0900

    START WORLD

---
+++ index01.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="refresh" content="2; URL=">
+    <link rel="stylesheet" href="./css/master.min.css">
+    <title>CONTACT/お問い合わせ</title>
+</head>
+<body>
+<table>
+    <tr>
+        <td class="menu">
+            <?php include('./php/menu.php');?>
+        </td>
+        <td clas="main">
+          <?php include('./php/contact.php');?>
+        </td>
+    </tr>
+</table>
+</body>
+</html>
